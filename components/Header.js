3bb6b3bf8fcbeaaaf51a0de31d6df662e67a125a
aseedk <71330292+aseedk@users.noreply.github.com>
2022-01-26 11:34:50 +0500

    Airbnb Clone

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,48 @@
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 import Image from "next/image";
 import {SearchIcon, GlobeAltIcon, UserCircleIcon, UsersIcon, MenuIcon} from "@heroicons/react/solid";
+import {useState} from "react";
+import {useRouter} from "next/router";
 
-function Header() {
+function Header({placeholder}) {
+    const [searchInput, setSearchInput] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [noOfGuests, setNoOfGuests] = useState(1);
+    const router = useRouter();
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+    const selectionRange = {
+        startDate: startDate ,
+        endDate: endDate,
+        key: 'selection'
+    }
+    const resetInput = () => {
+        setSearchInput('');
+        setNoOfGuests(1);
+        setEndDate(new Date());
+    }
+    const handleSearch = () => {
+      router.push({
+          pathname: '/search',
+          query: {
+              location: searchInput,
+              startDate: startDate.toISOString(),
+              endDate: endDate.toISOString(),
+              noOfGuests: noOfGuests
+          }
+      });
+    }
     return(
         <header className={'sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 '}>
         {/*Left */}
-            <div className={'relative flex items-center h-10 cursor-pointer my-auto'}>
+            <div className={'relative flex items-center h-10 cursor-pointer my-auto'}
+                 onClick={()=> router.push("/")}
+            >
                 {/* eslint-disable-next-line jsx-a11y/alt-text */}
                 <Image src={'https://links.papareact.com/qd3'}
                        layout={'fill'}
@@ -14,11 +51,12 @@ function Header() {
                 />
             </div>
         {/*Middle*/}
-            <div className={'flex items-center md:border-2 rounded-full py-2 md:shadow-lg text-sm text-gray-400'}>
+            <div className={'flex items-center md:border-2 rounded-full py-2 md:shadow-lg text-sm text-gray-900'}>
                 <input type={'text'}
-                       placeholder={"Start your search"}
+                       placeholder={placeholder || "Start your search"}
                        className={"pl-5 bg-transparent outline-none flex-grow"}
-
+                       value={searchInput}
+                       onChange={(event => setSearchInput(event.target.value))}
                 />
                 <SearchIcon className={'hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'}/>
             </div>
@@ -31,6 +69,38 @@ function Header() {
                     <UserCircleIcon className={'h-6'}/>
                 </div>
             </div>
+            {searchInput && (
+                <div className={'flex flex-col col-span-3 mx-auto'}>
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={['#FD5B61']}
+                        onChange={handleSelect}
+                    />
+                    <div className={'flex items-center border-b mb-4'}>
+                        <h2 className={'text-2xl flex-grow font-semibold'}>Number of Guests</h2>
+                        <UsersIcon className={"h-5"} />
+                        <input type={'number'}
+                               className={'w-12 pl-2 text-lg outline-none text-red-400'}
+                               value={noOfGuests}
+                               min={1}
+                               onChange={event => setNoOfGuests(event.target.value)}
+                        />
+                    </div>
+                    <div className={'flex'}>
+                        <button className={'flex-grow text-gray-500'}
+                                onClick={resetInput}
+                        >
+                            Cancel
+                        </button>
+                        <button className={'flex-grow text-red-400'}
+                                onClick={handleSearch}
+                        >
+                            Search
+                        </button>
+                    </div>
+                </div>
+            )}
         </header>
     )
 }
