7bbeaf92b200776ad8f4c2e27d15683085777f58
aseedk <71330292+aseedk@users.noreply.github.com>
2022-02-04 15:26:15 +0500

    Project Completed

--- components/Header.js
+++ components/Header.js
@@ -41,9 +41,9 @@ function Header({placeholder}) {
       });
     }
     return(
-        <header className={'sticky top-0 z-50 flex sm:grid sm:grid-cols-3 items-center justify-between bg-[#f7f7f7] shadow-md p-5 md:px-10 w-[100%] '}>
+        <header className={'sticky top-0 z-50 flex md:grid md:grid-cols-3 items-center justify-between bg-[#f7f7f7] shadow-md p-5 md:px-10 w-[100%] '}>
         {/*Left */}
-            <div className={'sm:hidden'}>
+            <div className={'md:hidden'}>
                 <div onClick={()=>{
                     setShowSearch(!showSearch);
                 }}>
@@ -64,12 +64,12 @@ function Header({placeholder}) {
                 />
                 <h1 className={'text-2xl ml-2 font-semibold family-Roboto'}>Techdev</h1>
             </div>
-            <div className={'sm:hidden'}>
+            <div className={'md:hidden'}>
                 <MenuIcon className={'h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'}/>
             </div>
         {/*Middle*/}
             <div style={{display: showSearch && 'flex'}}
-                 className={'flex hidden absolute h-3/5 w-4/5 sm:h-full shadow-lg border-1 sm:relative bg-white w-full  sm:inline-flex items-center md:border-2 rounded-full py-2 md:shadow-lg text-sm text-gray-900'}>
+                 className={'flex hidden absolute h-3/5 w-4/5 md:h-full shadow-lg border-1 md:relative bg-white w-full  md:inline-flex items-center md:border-2 rounded-full py-2 lg:shadow-lg text-sm text-gray-900'}>
                 { showCross && (
                     <div onClick={()=>{
                         setShowSearch(false);
@@ -83,7 +83,7 @@ function Header({placeholder}) {
 
                 <input type={'text'}
                        placeholder={placeholder || "Start your search"}
-                       className={"pl-5 bg-transparent outline-none flex-grow"}
+                       className={"pl-5 w-3/4 bg-transparent outline-none flex-grow"}
                        value={searchInput}
                        onChange={(event => {
                            setSearchInput(event.target.value);
@@ -97,7 +97,7 @@ function Header({placeholder}) {
                 <SearchIcon className={'inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2'}/>
             </div>
         {/*Right*/}
-            <div className={'flex hidden sm:inline-flex items-center space-x-4 justify-end text-gray-500'}>
+            <div className={'flex hidden md:inline-flex items-center space-x-4 justify-end text-gray-500'}>
                 <p className={'md:inline-flex cursor-pointer'}
                    onClick={() => {router.push({
                        pathname: '/map',
@@ -112,7 +112,7 @@ function Header({placeholder}) {
                 </div>
             </div>
             {searchInput && (
-                <div className={'absolute  md:w-fit bottom-0 translate-y-full md:translate-y-0 bg-white md:relative md:flex md:flex-col md:col-span-3 mx-auto mt-5 px-4 py-8'}>
+                <div className={'absolute  md:w-fit bottom-0 translate-y-full lg:translate-y-0 bg-white md:relative md:flex md:flex-col md:col-span-3 mx-auto mt-5 px-4 py-8'}>
                     <div className={'hidden md:inline-flex'}>
                         <DateRangePicker
                             ranges={[selectionRange]}
