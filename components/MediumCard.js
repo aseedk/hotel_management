dd09243362610203688a789296ac2aa81ef7d5b4
aseedk <71330292+aseedk@users.noreply.github.com>
2022-02-08 12:36:44 +0500

    Project Complete

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import {useRouter} from "next/router";
 const MediumCard = ({img, location, distance}) => {
-  return(
-      <div className={'cursor-pointer hover:scale-105 transition transform duration-300 ease-out mb-5'}>
+  const router = useRouter();
+    return(
+      <div className={'cursor-pointer hover:scale-105 transition transform duration-300 ease-out mb-5'}
+           onClick={()=> router.push({
+               pathname: 'detail',
+               query: {id:'123'}
+           })}
+      >
         <div className={'relative h-80 w-80'}>
           {/* eslint-disable-next-line jsx-a11y/alt-text */}
           <Image src={img}
